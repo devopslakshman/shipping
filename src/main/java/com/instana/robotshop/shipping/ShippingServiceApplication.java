c3d2d28cfa55235cea684a0f5d1988f2bafe40d7
devopslakshman <lakshmandevops5152@gmail.com>
2026-09-08 12:52:35 +0530

    Update ShippingServiceApplication.java

--- src/main/java/com/instana/robotshop/shipping/ShippingServiceApplication.java
+++ src/main/java/com/instana/robotshop/shipping/ShippingServiceApplication.java
@@ -1,13 +1,10 @@
 package com.instana.robotshop.shipping;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
-
 import com.instana.sdk.support.SpanSupport;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Bean;
@@ -18,14 +15,11 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
-
 import java.util.Random;
-
 @SpringBootApplication
 @EnableRetry
 @EnableWebMvc
 public class ShippingServiceApplication implements WebMvcConfigurer {
-
     private static final String[] DATA_CENTERS = {
             "asia-northeast2",
             "asia-south1",
@@ -33,16 +27,14 @@ public class ShippingServiceApplication implements WebMvcConfigurer {
             "us-east1",
             "us-west1"
     };
-
+    private static final Random RANDOM = new Random();
     public static void main(String[] args) {
         SpringApplication.run(ShippingServiceApplication.class, args);
     }
-
     @Bean
     public BeanPostProcessor dataSourceWrapper() {
         return new DataSourcePostProcessor();
     }
-
     @Order(Ordered.HIGHEST_PRECEDENCE)
     private static class DataSourcePostProcessor implements BeanPostProcessor {
         @Override
@@ -52,24 +44,19 @@ public class ShippingServiceApplication implements WebMvcConfigurer {
             }
             return bean;
         }
-
         @Override
         public Object postProcessAfterInitialization(Object bean, String name) throws BeansException {
             return bean;
         }
     }
-
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new InstanaDatacenterTagInterceptor());
     }
-
     private static class InstanaDatacenterTagInterceptor extends HandlerInterceptorAdapter {
         @Override
         public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-
-            SpanSupport.annotate("datacenter", DATA_CENTERS[new Random().nextInt(DATA_CENTERS.length)]);
-
+            SpanSupport.annotate("datacenter", DATA_CENTERS[RANDOM.nextInt(DATA_CENTERS.length)]);
             return super.preHandle(request, response, handler);
         }
     }
